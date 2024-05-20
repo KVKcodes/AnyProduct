3a48fbdba833ea78bd88a07eb44dcd45d5e9a092
KVK Siddartha <sidduultralegend@gmail.com>
2024-05-20 16:41:54 +0530

    added build.sh

--- producter/settings.py
+++ producter/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'producter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
